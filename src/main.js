29c1048876f85e9f0c685a70568afa6b433560d6
gor <gorsayadyan1@gmail.com>
2023-08-14 15:08:38 +0300

    add button and sort view

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import './views/brief-view.js';
 import './views/filter-view.js';
+import './views/add-button-view.js';
+import './views/sort-view.js';
 
 
 /**
@@ -12,6 +14,18 @@ const briefView = document.querySelector('brief-view');
  */
 const filterView = document.querySelector('filter-view');
 
+/**
+ * @type { import('./views/add-button-view.js').default }
+ */
+const addButtonView = document.querySelector('add-button-view');
+
+/**
+ * @type { import('./views/sort-view.js').default }
+ */
+const sortView = document.querySelector('sort-view');
+
+sortView.render();
+addButtonView.render();
 briefView.render();
 filterView.render();
 
