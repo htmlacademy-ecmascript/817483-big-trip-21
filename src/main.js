4eed547cbfc461384342141ae0a7adfafdd9cb0e
gor <gorsayadyan1@gmail.com>
2023-08-18 00:08:41 +0300

    add presenters for every views

--- src/main.js
+++ src/main.js
@@ -3,13 +3,23 @@ import './views/filter-view.js';
 import './views/add-button-view.js';
 import './views/sort-view.js';
 import './views/list-view.js';
+
 import AppModel from './models/app-model.js';
+
+import BriefPresenter from './presenters/brief-presenter.js';
+import FilterPresenter from './presenters/filter-presenter.js';
+import AddButtonPresenter from './presenters/add-button-presenter.js';
+import SortPresenter from './presenters/sort-presenter.js';
+import ListPresenter from './presenters/list-presenter.js';
+
 
 const appModel = new AppModel();
 appModel.ready().then(() => {
-  console.log(appModel.getPoints());
-  console.log(appModel.getDestinations());
-  console.log(appModel.getOfferGroups());
+  new BriefPresenter(document.querySelector('brief-view'), appModel);
+  new FilterPresenter(document.querySelector('filter-view'), appModel);
+  new AddButtonPresenter(document.querySelector('add-button-view'), appModel);
+  new SortPresenter(document.querySelector('sort-view'), appModel);
+  new ListPresenter(document.querySelector('list-view'), appModel);
 });
 /**
  * @type { import('./views/brief-view.js').default }
---
+++ src/presenters/presenter.js
@@ -0,0 +1,24 @@
+
+/**
+ * @template {import('../views/views').default} View
+ * @template {import('../models/model').default} Model
+ */
+class Presenter {
+  /**
+   * @param {View} view
+   * @param {Model} model
+   */
+  constructor(view, model) {
+    this.view = view;
+    this.model = model;
+
+    window.queueMicrotask(() => this.updateView());
+  }
+
+  /**
+   * @abstract
+   */
+  updateView() {}
+}
+
+export default Presenter;
