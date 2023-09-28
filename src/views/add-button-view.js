6e27d998f3b011b73636fe7a1bab81bb7e273a59
gor <gorsayadyan1@gmail.com>
2023-09-28 13:17:01 +0300

    Научили addbuttonview принимать данные

--- src/views/add-button-view.js
+++ src/views/add-button-view.js
@@ -1,13 +1,26 @@
 import View from './views.js';
 import {html} from '../utils.js';
 
+/**
+ * @typedef {{
+*  isDisabled: boolean
+* }} State
+* @extends {View<State>}
+*/
 class AddButtonView extends View {
   /**
    * @override
    */
   createHtml() {
+    const {isDisabled} = this.state;
+
     return html`
-    <button class="trip-main__event-add-btn  btn  btn--big  btn--yellow" type="button" disabled="">New event</button>
+    <button
+      class="trip-main__event-add-btn  btn  btn--big  btn--yellow"
+      type="button"
+      ${isDisabled ? 'disabled' : ''}>
+      New event
+    </button>
     `;
   }
 }
