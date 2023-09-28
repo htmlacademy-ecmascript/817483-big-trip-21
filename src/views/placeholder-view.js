e0663f730508cb193e4fb1722f4f1ac08db4b4f6
gor <gorsayadyan1@gmail.com>
2023-09-28 10:10:02 +0300

    Placeholder принимает данные

--- src/views/placeholder-view.js
+++ src/views/placeholder-view.js
@@ -1,14 +1,26 @@
 import View from './views.js';
 import {html} from '../utils.js';
 
+/**
+ * @typedef {{
+ *  message: string
+ * }} State
+ * @extends {View<State>}
+ */
 class PlaceholderView extends View {
 
   /**
    * @override
    */
   createHtml() {
+    const {message} = this.state;
+
+    if(!message) {
+      return '';
+    }
+
     return html`
-      <p class="trip-events__msg">Click New Event to create your first point</p>
+      <p class="trip-events__msg">${message}</p>
     `;
   }
 }
