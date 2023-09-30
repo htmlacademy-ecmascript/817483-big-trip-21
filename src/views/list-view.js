e030fae19ed958e41de08f5d6f1a2a7d6ff19d11
gor <gorsayadyan1@gmail.com>
2023-09-30 15:58:20 +0300

    вернули listview исходные css классы

--- src/views/list-view.js
+++ src/views/list-view.js
@@ -39,7 +39,7 @@ class ListView extends View {
   constructor() {
     super();
 
-    this.classList.add('trip-list');
+    this.classList.add('trip-events__list');
     this.setAttribute('role', 'list');
   }
 
@@ -51,7 +51,7 @@ class ListView extends View {
 
       const view = item.isEditable ? new EditorView() : new CardView();
 
-      view.classList.add('trip-list__item');
+      view.classList.add('trip-events__item');
       view.setAttribute('role', 'listitem');
       view.setState(item);
 
