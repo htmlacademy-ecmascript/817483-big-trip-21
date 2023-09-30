11d7a1a97579b8773db95fadbd398a9c6e44cafe
gor <gorsayadyan1@gmail.com>
2023-09-30 14:36:30 +0300

    Добавили функцию formatDateRange

--- src/utils.js
+++ src/utils.js
@@ -54,11 +54,29 @@ function sanitize(data) {
 }
 
 /**
- * @param {dayjs.ConfigType} value
+ * @param {dayjs.ConfigType} valueFrom
+ * @param {dayjs.ConfigType} valueTo
  * @returns {string}
  */
-function formatDate(value) {
-  return dayjs(value).format('MMM D');
+function formatDateRange(valueFrom, valueTo) {
+  valueFrom = dayjs(valueFrom);
+  valueTo = dayjs(valueTo);
+  if(valueFrom.isSame(valueTo, 'day')) {
+    return formatDate(valueFrom);
+  }
+
+  return [
+    formatDate(valueFrom, valueFrom.isSame(valueTo, 'month')),
+    formatDate(valueTo)
+  ].join(' - ');
+}
+/**
+ * @param {dayjs.ConfigType} value
+ * @param {boolean} [isNarrow]
+ * @returns {string}
+ */
+function formatDate(value, isNarrow) {
+  return dayjs(value).format(isNarrow ? 'D' : 'D MMM');
 }
 
 /**
@@ -139,6 +157,7 @@ function createCalendars(inputFrom, inputTo) {
 
 export {
   html,
+  formatDateRange,
   formatDate,
   formatTime,
   formatDuration,
